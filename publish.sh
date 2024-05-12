ee08c56ede0654ad22fbb1fbc38dfd70eabfefe7
PartMan <mane.parth@gmail.com>
2024-05-13 00:14:57 +0530

    chore: Use git remotes

--- publish.sh
+++ publish.sh
@@ -3,8 +3,6 @@
 set -e
 
 GIT_BRANCH="$(git name-rev --name-only HEAD)"
-PUBLIC_REPO="https://github.com/PartMan7/PartBot-spoof.git"
-SECRET_REPO="https://github.com/PartMan7/PartBot-secrets.git"
 
 case $1 in
   public)
@@ -16,14 +14,14 @@ case $1 in
       exit 1
     fi
 
-    git fetch $PUBLIC_REPO main
-    if git log FETCH_HEAD..@ --format='format:%s' | grep -Pq 'secrets?:'
+    git fetch public main
+    if git log public/main..@ --format='format:%s' | grep -Pq 'secrets?:'
     then
       echo Found a commit with "'secrets'" in the name\; failing publish...
       exit 1
     fi
     git rebase FETCH_HEAD
-    git push $PUBLIC_REPO main
+    git push public main
   ;;
 
   secrets)
@@ -35,16 +33,17 @@ case $1 in
       exit 1
     fi
 
-    git fetch $PUBLIC_REPO main
-    git rebase FETCH_HEAD
-    git fetch $SECRET_REPO main
-    git rebase FETCH_HEAD
-    if git log FETCH_HEAD..@ --format='format:%s' | grep -vPq 'secrets?:'
+    git fetch public main
+    git rebase public/main
+    git fetch secret main
+    git rebase public/main secret/main
+    git rebase secret/main secrets
+    if git log secret/main..@ --format='format:%s' | grep -vPq 'secrets?:'
     then
       echo Found a commit without "'secrets'" in the name\; failing publish... \(all commits must include the word "'secrets'"\)
       exit 1
     fi
-    git push $SECRET_REPO secrets:main --force # Lease won't work here because we already updated the HEAD earlier
+    git push secret secrets:main --force # Lease won't work here because we already updated the HEAD earlier
   ;;
 
   *)
