0e8cd6ca6db2aed4dd1b16eda07bcc0e5f5464d1
PartMan <mane.parth@gmail.com>
2024-05-12 23:56:32 +0530

    chore: Use local branch to determine where to publish

--- publish.sh
+++ publish.sh
@@ -10,28 +10,40 @@ case $1 in
   public)
     echo Publishing to public repo...
 
-    git fetch $PUBLIC_REPO $GIT_BRANCH
+    if [[ "$GIT_BRANCH" == secrets ]]
+    then
+      echo On secrets branch - aborting
+      exit 1
+    fi
+
+    git fetch $PUBLIC_REPO main
     if git log FETCH_HEAD..@ --format='format:%s' | grep -Pq 'secrets?:'
     then
       echo Found a commit with "'secrets'" in the name\; failing publish...
       exit 1
     fi
     git rebase FETCH_HEAD
-    git push $PUBLIC_REPO $GIT_BRANCH
+    git push $PUBLIC_REPO main
   ;;
 
   secrets)
     echo Publishing to SECRET repo!
 
-    git fetch $SECRET_REPO $GIT_BRANCH
+    if [[ "$GIT_BRANCH" != secrets ]]
+    then
+      echo Not on secrets branch - aborting
+      exit 1
+    fi
+
+    git fetch $SECRET_REPO main
     git rebase FETCH_HEAD
     if git log FETCH_HEAD..@ --format='format:%s' | grep -vPq 'secrets?:'
     then
       echo Found a commit without "'secrets'" in the name\; failing publish... \(all commits must include the word "'secrets'"\)
       exit 1
     fi
-    git fetch $PUBLIC_REPO $GIT_BRANCH
+    git fetch $PUBLIC_REPO main
     git rebase FETCH_HEAD
-    git push $SECRET_REPO $GIT_BRANCH --force-with-lease
+    git push $SECRET_REPO main --force-with-lease
   ;;
 esac
