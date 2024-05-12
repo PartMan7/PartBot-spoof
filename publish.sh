1b5e8bfa324f5a3b28e1ce67f2fb23583c8569ef
PartMan <mane.parth@gmail.com>
2024-05-12 23:47:23 +0530

    chore: Fix git grep

--- publish.sh
+++ publish.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-set -x
-
 GIT_BRANCH="$(git name-rev --name-only HEAD)"
 PUBLIC_REPO="https://github.com/PartMan7/PartBot-spoof.git"
 SECRET_REPO="https://github.com/PartMan7/PartBot-secrets.git"
@@ -11,7 +9,7 @@ case $1 in
     echo Publishing to public repo...
 
     git fetch $PUBLIC_REPO $GIT_BRANCH
-    if git log FETCH_HEAD..@ --format='format:%s' | grep -q 'secrets?:'
+    if git log FETCH_HEAD..@ --format='format:%s' | grep -Pq 'secrets?:'
     then
       echo Found a commit with "'secrets'" in the name\; failing publish...
       exit 1
@@ -25,7 +23,7 @@ case $1 in
 
     git fetch $SECRET_REPO $GIT_BRANCH
     git rebase FETCH_HEAD
-    if git log FETCH_HEAD..@ --format='format:%s' | grep -vq 'secrets?:'
+    if git log FETCH_HEAD..@ --format='format:%s' | grep -vPq 'secrets?:'
     then
       echo Found a commit without "'secrets'" in the name\; failing publish... \(all commits must include the word "'secrets'"\)
     fi
