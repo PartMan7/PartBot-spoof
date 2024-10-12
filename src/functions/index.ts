bfc8fae821e897b9bafae463145398b2103b687a
Parth Mane <parth.mane@sprinklr.com>
2024-10-12 17:09:00 +0530

    refactor: Renaming to @
    See: https://github.com/PartMan7/PartBotter/pull/17

--- src/functions/index.ts
+++ src/functions/index.ts
@@ -1,4 +1,4 @@
-import * as SecretFunctions from 'secrets/functions/providers';
+import * as SecretFunctions from '@/secrets/functions/providers';
 
 export default function getSecretFunction<T> (lookup: string, fallback: T): T {
 	if (lookup in SecretFunctions) return SecretFunctions[lookup];
